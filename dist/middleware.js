a7177eb145b84e4c842d2f604506e2bd882beee7
Marcus Freire <vinny.freire@gmail.com>
2017-09-15 11:36:32 -0300

    add logger

--- dist/middleware.js
+++ dist/middleware.js
@@ -13,6 +13,10 @@ var _koaConvert = require('koa-convert');
 
 var _koaConvert2 = _interopRequireDefault(_koaConvert);
 
+var _koaLogger = require('koa-logger');
+
+var _koaLogger2 = _interopRequireDefault(_koaLogger);
+
 var _koaCors = require('koa-cors');
 
 var _koaCors2 = _interopRequireDefault(_koaCors);
@@ -24,5 +28,5 @@ var _koaBodyparser2 = _interopRequireDefault(_koaBodyparser);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function middleware() {
-    return (0, _koaCompose2.default)([(0, _koaConvert2.default)((0, _koaCors2.default)()), (0, _koaConvert2.default)((0, _koaBodyparser2.default)())]);
+    return (0, _koaCompose2.default)([(0, _koaLogger2.default)(), (0, _koaConvert2.default)((0, _koaCors2.default)()), (0, _koaConvert2.default)((0, _koaBodyparser2.default)())]);
 }
