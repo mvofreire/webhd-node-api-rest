f6e0c2f89f1044d1539910a275a405803e4fdfe5
Marcus Freire <vinny.freire@gmail.com>
2017-09-15 11:13:36 -0300

    dist

--- dist/index.js
+++ dist/index.js
@@ -24,6 +24,26 @@ var _koaJson = require('koa-json');
 
 var _koaJson2 = _interopRequireDefault(_koaJson);
 
+var _koaCompose = require('koa-compose');
+
+var _koaCompose2 = _interopRequireDefault(_koaCompose);
+
+var _koaConvert = require('koa-convert');
+
+var _koaConvert2 = _interopRequireDefault(_koaConvert);
+
+var _koaLogger = require('koa-logger');
+
+var _koaLogger2 = _interopRequireDefault(_koaLogger);
+
+var _koaCors = require('koa-cors');
+
+var _koaCors2 = _interopRequireDefault(_koaCors);
+
+var _koaBodyparser = require('koa-bodyparser');
+
+var _koaBodyparser2 = _interopRequireDefault(_koaBodyparser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var ApiRest = {
@@ -37,8 +57,16 @@ var ApiRest = {
                 //json
                 app.use((0, _koaJson2.default)());
 
+                //json
+                app.use((0, _koaCompose2.default)([(0, _koaLogger2.default)(), (0, _koaConvert2.default)((0, _koaCors2.default)()), (0, _koaConvert2.default)((0, _koaBodyparser2.default)())]));
+
                 //register routes
                 app.use(_routes2.default.registerRoutes());
+
+                //404
+                app.use(function (ctx) {
+                        return ctx.status = 404;
+                });
 
                 console.log('server listenig on port localhost:' + _config.server.port);
                 app.listen(_config.server.port);
--- src/index.js
+++ src/index.js
@@ -31,6 +31,9 @@ const ApiRest = {
         //register routes
         app.use(ApiRoutes.registerRoutes())
 
+        //404
+        app.use(ctx => ctx.status = 404)
+
         console.log(`server listenig on port localhost:${_config.server.port}`);
         app.listen(_config.server.port);
     }
