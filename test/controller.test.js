cbf821f69e86125c5252daf915cf749fb44746c0
Marcus Freire <vinny.freire@gmail.com>
2017-09-15 07:51:22 -0300

    add test controller

---
+++ test/controller.test.js
@@ -0,0 +1,153 @@
+'use strict'
+
+import { expect } from 'chai'
+import pagination from './index'
+
+test('pagination should be a function', () => {
+  expect(pagination).to.be.a('function')
+})
+
+// test('pagination({ total: 1, activePage: 1 }) should return [1]', () => {
+//   const params = { total: 1, activePage: 1 }
+//   const result = [1]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('pagination({ total: 2, activePage: 1 }) should return [1, 2]', () => {
+//   const params = { total: 2, activePage: 1 }
+//   const result = [1, 2]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 5, activePage: 1 }) should return [1, 2, 3, 4, 5]', () => {
+//   const params = { total: 5, activePage: 1 }
+//   const result = [1, 2, 3, 4, 5]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 6, activePage: 1 }) should return [1, 2, 3, "...", 6]', () => {
+//   const params = { total: 6, activePage: 1 }
+//   const result = [1, 2, 3, '...', 6]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 6, activePage: 2 }) should return [1, 2, 3, "...", 6]', () => {
+//   const params = { total: 6, activePage: 2 }
+//   const result = [1, 2, 3, '...', 6]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 6, activePage: 3 }) should return [1, 2, 3, 4, 5, 6]', () => {
+//   const params = { total: 6, activePage: 3 }
+//   const result = [1, 2, 3, 4, 5, 6]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 6, activePage: 4 }) should return [1, 2, 3, 4, 5, 6]', () => {
+//   const params = { total: 6, activePage: 4 }
+//   const result = [1, 2, 3, 4, 5, 6]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 6, activePage: 5 }) should return [1, "...", 4, 5, 6]', () => {
+//   const params = { total: 6, activePage: 5 }
+//   const result = [1, '...', 4, 5, 6]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 6, activePage: 6 }) should return [1, "..." , 4, 5, 6]', () => {
+//   const params = { total: 6, activePage: 6 }
+//   const result = [1, '...', 4, 5, 6]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 7, activePage: 1 }) should return [1, 2, 3, 4, "...", 7]', () => {
+//   const params = { total: 7, activePage: 1 }
+//   const result = [1, 2, 3, '...', 7]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 7, activePage: 3 }) should return [1, 2, 3, 4, "...", 7]', () => {
+//   const params = { total: 7, activePage: 3 }
+//   const result = [1, 2, 3, 4, '...', 7]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 7, activePage: 4 }) should return [1, 2, 3, 4, 5, 6, 7]', () => {
+//   const params = { total: 7, activePage: 4 }
+//   const result = [1, 2, 3, 4, 5, 6, 7]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 7, activePage: 5 }) should return [1, "...", 4, 5, 6, 7]', () => {
+//   const params = { total: 7, activePage: 5 }
+//   const result = [1, '...', 4, 5, 6, 7]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 7, activePage: 6 }) should return [1, "...", 5, 6, 7]', () => {
+//   const params = { total: 7, activePage: 6 }
+//   const result = [1, '...', 5, 6, 7]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 7, activePage: 7 }) should return [1, "...", 5, 6, 7]', () => {
+//   const params = { total: 7, activePage: 7 }
+//   const result = [1, '...', 5, 6, 7]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 15, activePage: 8 }) should return [1, "...", 7, 8, 9, "...", 15]', () => {
+//   const params = { total: 15, activePage: 8 }
+//   const result = [1, '...', 7, 8, 9, '...', 15]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: 15 }) should return [1, 2, 3, "...", 15]', () => {
+//   const params = { total: 15 }
+//   const result = [1, 2, 3, '...', 15]
+
+//   expect(pagination(params)).to.be.deep.equal(result)
+// })
+
+// test('paginaton() should return [1]', () => {
+//   const result = [1]
+
+//   expect(pagination()).to.be.deep.equal(result)
+// })
+
+// test('paginaton({ total: "abc", activePage: 1 }) should throw an error', () => {
+//   const params = { total: "abc", activePage: 1 }
+//   const result = 'total should be a number'
+//   try {
+//     pagination(params)
+//   } catch (e){
+//     expect(e.message).to.be.equal(result)
+//   }
+// })
+
+// test('paginaton({ total: 10, activePage: "1a" }) should throw an error', () => {
+//   const params = { total: 10, activePage: "1a" }
+//   const result = 'activePage should be a number'
+//   try {
+//     pagination(params)
+//   } catch (e){
+//     expect(e.message).to.be.equal(result)
+//   }
+// })
