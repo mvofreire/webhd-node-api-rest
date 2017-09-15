e79246419589928c405cc8495e8a270c2286da33
Marcus Freire <vinny.freire@gmail.com>
2017-09-15 11:22:12 -0300

    remove logger

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,12 @@
 import compose from 'koa-compose'
 import convert from 'koa-convert'
-import logger from 'koa-logger'
+// import logger from 'koa-logger'
 import cors from 'koa-cors'
 import bodyParser from 'koa-bodyparser'
 
 export default function middleware() {
     return compose([
-        logger(),
+        //logger(),
         convert(cors()),
         convert(bodyParser()),
     ])
